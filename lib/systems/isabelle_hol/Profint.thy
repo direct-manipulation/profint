c09aa1c26700c2c9ef4a285659c514d9158c17c0
Kaustuv Chaudhuri <kaustuv.chaudhuri@inria.fr>
2022-09-19 03:02:26 +0200

    chore(lib/systems/isabelle_hol): use xsymbol instead of ASCII

--- lib/systems/isabelle_hol/Profint.thy
+++ lib/systems/isabelle_hol/Profint.thy
@@ -4,159 +4,159 @@ begin
 
 (* These are the transport theorems *)
 
-theorem go_left_and: "(a --> b) --> (a & c --> b & c)"
+theorem go_left_and: "(a \<longrightarrow> b) \<longrightarrow> (a \<and> c \<longrightarrow> b \<and> c)"
   by blast
-theorem go_right_and: "(a --> b) --> (c & a --> c & b)"
+theorem go_right_and: "(a \<longrightarrow> b) \<longrightarrow> (c \<and> a \<longrightarrow> c \<and> b)"
   by blast
-theorem go_left_or: "(a --> b) --> (a | c --> b | c)"
+theorem go_left_or: "(a \<longrightarrow> b) \<longrightarrow> (a \<or> c \<longrightarrow> b \<or> c)"
   by blast
-theorem go_right_or: "(a --> b) --> (c | a --> c | b)"
+theorem go_right_or: "(a \<longrightarrow> b) \<longrightarrow> (c \<or> a \<longrightarrow> c \<or> b)"
   by blast
-theorem go_left_imp: "(b --> a) --> ((a --> c) --> (b --> c))"
+theorem go_left_imp: "(b \<longrightarrow> a) \<longrightarrow> ((a \<longrightarrow> c) \<longrightarrow> (b \<longrightarrow> c))"
   by blast
-theorem go_right_imp: "(a --> b) --> ((c --> a) --> (c --> b))"
+theorem go_right_imp: "(a \<longrightarrow> b) \<longrightarrow> ((c \<longrightarrow> a) \<longrightarrow> (c \<longrightarrow> b))"
   by blast
-theorem go_down_all: "(ALL x. p x --> q x) --> (ALL x. p x) --> (ALL x. q x)"
+theorem go_down_all: "(\<forall> x. p x \<longrightarrow> q x) \<longrightarrow> (\<forall> x. p x) \<longrightarrow> (\<forall> x. q x)"
   by blast
-theorem go_down_ex: "(ALL x. p x --> q x) --> (EX x. p x) --> (EX x. q x)"
+theorem go_down_ex: "(\<forall> x. p x \<longrightarrow> q x) \<longrightarrow> (\<exists> x. p x) \<longrightarrow> (\<exists> x. q x)"
   by blast
-theorem init: "True --> (a --> a)"
+theorem init: "True \<longrightarrow> (a \<longrightarrow> a)"
   by blast
 
 (* These are the rule names *)
 
-theorem goal_ts_and_l: "((a --> b) & c) --> (a --> (b & c))"
+theorem goal_ts_and_l: "((a \<longrightarrow> b) \<and> c) \<longrightarrow> (a \<longrightarrow> (b \<and> c))"
   by blast
-theorem goal_ts_and_r: "(c & (a --> b)) --> (a --> (c & b))"
+theorem goal_ts_and_r: "(c \<and> (a \<longrightarrow> b)) \<longrightarrow> (a \<longrightarrow> (c \<and> b))"
   by blast
-theorem goal_and_ts_l: "(a --> b) --> (a & c --> b)"
+theorem goal_and_ts_l: "(a \<longrightarrow> b) \<longrightarrow> (a \<and> c \<longrightarrow> b)"
   by blast
-theorem goal_and_ts_r: "(a --> b) --> (c & a --> b)"
+theorem goal_and_ts_r: "(a \<longrightarrow> b) \<longrightarrow> (c \<and> a \<longrightarrow> b)"
   by blast
-theorem goal_ts_or_l: "(a --> b) --> (a --> b | c)"
+theorem goal_ts_or_l: "(a \<longrightarrow> b) \<longrightarrow> (a \<longrightarrow> b \<or> c)"
   by blast
-theorem goal_ts_or_r: "(a --> b) --> (a --> c | b)"
+theorem goal_ts_or_r: "(a \<longrightarrow> b) \<longrightarrow> (a \<longrightarrow> c \<or> b)"
   by blast
-theorem goal_or_ts: "((a --> c) & (b --> c)) --> (a | b --> c)"
+theorem goal_or_ts: "((a \<longrightarrow> c) \<and> (b \<longrightarrow> c)) \<longrightarrow> (a \<or> b \<longrightarrow> c)"
   by blast
-theorem goal_ts_imp_l: "(a & b --> c) --> (a --> b --> c)"
+theorem goal_ts_imp_l: "(a \<and> b \<longrightarrow> c) \<longrightarrow> (a \<longrightarrow> b \<longrightarrow> c)"
   by blast
-theorem goal_ts_imp_r: "(c --> a --> b) --> (a --> c --> b)"
+theorem goal_ts_imp_r: "(c \<longrightarrow> a \<longrightarrow> b) \<longrightarrow> (a \<longrightarrow> c \<longrightarrow> b)"
   by blast
-theorem goal_imp_ts: "(c & (a --> b)) --> ((c --> a) --> b)"
+theorem goal_imp_ts: "(c \<and> (a \<longrightarrow> b)) \<longrightarrow> ((c \<longrightarrow> a) \<longrightarrow> b)"
   by blast
-theorem goal_ts_all: "(ALL x. (a --> p x)) --> (a --> (ALL x. p x))"
+theorem goal_ts_all: "(\<forall> x. (a \<longrightarrow> p x)) \<longrightarrow> (a \<longrightarrow> (\<forall> x. p x))"
   by blast
-theorem goal_all_ts: "(EX x. (p x --> b)) --> (ALL x. p x) --> b"
+theorem goal_all_ts: "(\<exists> x. (p x \<longrightarrow> b)) \<longrightarrow> (\<forall> x. p x) \<longrightarrow> b"
   by blast
-theorem goal_ts_ex: "(EX x. (a --> p x)) --> (a --> (EX x. p x))"
+theorem goal_ts_ex: "(\<exists> x. (a \<longrightarrow> p x)) \<longrightarrow> (a \<longrightarrow> (\<exists> x. p x))"
   by blast
-theorem goal_ex_ts: "(ALL x. (p x --> a)) --> (EX x. p x) --> a"
-  by blast
-
-theorem asms_and_l_l: "(a & (b & c)) --> (a & b)"
-  by blast
-theorem asms_and_l_r: "(a & (c & b)) --> (a & b)"
-  by blast
-theorem asms_and_r_l: "((a & c) & b) --> (a & b)"
-  by blast
-theorem asms_and_r_r: "((c & a) & b) --> (a & b)"
-  by blast
-theorem asms_or_l_l: "(a & (b | c)) --> ((a & b) | c)"
-  by blast
-theorem asms_or_l_r: "(a & (c | b)) --> (c | (a & b))"
-  by blast
-theorem asms_or_r_l: "((a | c) & b) --> ((a & b) | c)"
-  by blast
-theorem asms_or_r_r: "((c | a) & b) --> (c | (a & b))"
-  by blast
-theorem asms_imp_l_l: "(a & (b --> c)) --> ((a --> b) --> c)"
-  by blast
-theorem asms_imp_l_r: "(a & (c --> b)) --> (c --> (a & b))"
-  by blast
-theorem asms_imp_r_l: "((a --> c) & b) --> ((b --> a) --> c)"
-  by blast
-theorem asms_imp_r_r: "((c --> a) & b) --> (c --> (a & b))"
-  by blast
-theorem asms_all_l: "(a & (ALL x. p x)) --> (ALL x. (a & p x))"
-  by blast
-theorem asms_all_r: "((ALL x. p x) & a) --> (ALL x. (p x & a))"
-  by blast
-theorem asms_ex_l: "(a & (EX x. p x)) --> (EX x. (a & p x))"
-  by blast
-theorem asms_ex_r: "((EX x. p x) & a) --> (EX x. (p x & a))"
+theorem goal_ex_ts: "(\<forall> x. (p x \<longrightarrow> a)) \<longrightarrow> (\<exists> x. p x) \<longrightarrow> a"
   by blast
 
-theorem contract: "(a --> a --> b) --> (a --> b)"
+theorem asms_and_l_l: "(a \<and> (b \<and> c)) \<longrightarrow> (a \<and> b)"
   by blast
-theorem weaken: "b --> (a --> b)"
+theorem asms_and_l_r: "(a \<and> (c \<and> b)) \<longrightarrow> (a \<and> b)"
   by blast
-theorem inst_r: "!! t. p t --> (EX x. p x)"
+theorem asms_and_r_l: "((a \<and> c) \<and> b) \<longrightarrow> (a \<and> b)"
   by blast
-theorem inst_l: "!! t. (ALL x. p x) --> p t"
+theorem asms_and_r_r: "((c \<and> a) \<and> b) \<longrightarrow> (a \<and> b)"
   by blast
-theorem rewrite_rtl: "!! s t. p s --> s = t --> p t"
+theorem asms_or_l_l: "(a \<and> (b \<or> c)) \<longrightarrow> ((a \<and> b) \<or> c)"
   by blast
-theorem rewrite_ltr: "!! s t. p t --> s = t --> p s"
+theorem asms_or_l_r: "(a \<and> (c \<or> b)) \<longrightarrow> (c \<or> (a \<and> b))"
   by blast
-
-theorem simp_goal_and_top: "a --> (a & True)"
+theorem asms_or_r_l: "((a \<or> c) \<and> b) \<longrightarrow> ((a \<and> b) \<or> c)"
   by blast
-theorem simp_goal_top_and: "a --> (True & a)"
+theorem asms_or_r_r: "((c \<or> a) \<and> b) \<longrightarrow> (c \<or> (a \<and> b))"
   by blast
-theorem simp_asms_and_top: "(a & True) --> a"
+theorem asms_imp_l_l: "(a \<and> (b \<longrightarrow> c)) \<longrightarrow> ((a \<longrightarrow> b) \<longrightarrow> c)"
   by blast
-theorem simp_asms_top_and: "(True & a) --> a"
+theorem asms_imp_l_r: "(a \<and> (c \<longrightarrow> b)) \<longrightarrow> (c \<longrightarrow> (a \<and> b))"
   by blast
-
-theorem simp_goal_or_top: "True --> (a | True)"
+theorem asms_imp_r_l: "((a \<longrightarrow> c) \<and> b) \<longrightarrow> ((b \<longrightarrow> a) \<longrightarrow> c)"
   by blast
-theorem simp_goal_top_or: "True --> (True | a)"
+theorem asms_imp_r_r: "((c \<longrightarrow> a) \<and> b) \<longrightarrow> (c \<longrightarrow> (a \<and> b))"
   by blast
-theorem simp_asms_or_top: "(a | True) --> True"
+theorem asms_all_l: "(a \<and> (\<forall> x. p x)) \<longrightarrow> (\<forall> x. (a \<and> p x))"
   by blast
-theorem simp_asms_top_or: "(True | a) --> True"
+theorem asms_all_r: "((\<forall> x. p x) \<and> a) \<longrightarrow> (\<forall> x. (p x \<and> a))"
   by blast
-
-theorem simp_goal_imp_top: "True --> (a --> True)"
+theorem asms_ex_l: "(a \<and> (\<exists> x. p x)) \<longrightarrow> (\<exists> x. (a \<and> p x))"
   by blast
-theorem simp_goal_top_imp: "a --> (True --> a)"
-  by blast
-theorem simp_asms_imp_top: "(a --> True) --> True"
-  by blast
-theorem simp_asms_top_imp: "(True --> a) --> a"
+theorem asms_ex_r: "((\<exists> x. p x) \<and> a) \<longrightarrow> (\<exists> x. (p x \<and> a))"
   by blast
 
-theorem simp_goal_and_bot: "False --> (a & False)"
+theorem contract: "(a \<longrightarrow> a \<longrightarrow> b) \<longrightarrow> (a \<longrightarrow> b)"
   by blast
-theorem simp_goal_bot_and: "False --> (False & a)"
+theorem weaken: "b \<longrightarrow> (a \<longrightarrow> b)"
   by blast
-theorem simp_asms_and_bot: "(a & False) --> False"
+theorem inst_r: "\<And> t. p t \<longrightarrow> (\<exists> x. p x)"
   by blast
-theorem simp_asms_bot_and: "(False & a) --> False"
+theorem inst_l: "\<And> t. (\<forall> x. p x) \<longrightarrow> p t"
   by blast
-
-theorem simp_goal_or_bot: "a --> (a | False)"
+theorem rewrite_rtl: "\<And> s t. p s \<longrightarrow> s = t \<longrightarrow> p t"
   by blast
-theorem simp_goal_bot_or: "a --> (False | a)"
-  by blast
-theorem simp_asms_or_bot: "(a | False) --> a"
-  by blast
-theorem simp_asms_bot_or: "(False | a) --> a"
+theorem rewrite_ltr: "\<And> s t. p t \<longrightarrow> s = t \<longrightarrow> p s"
   by blast
 
-theorem simp_goal_bot_imp: "True --> (False --> a)"
+theorem simp_goal_and_top: "a \<longrightarrow> (a \<and> True)"
   by blast
-theorem simp_asms_bot_imp: "(False --> a) --> True"
+theorem simp_goal_top_and: "a \<longrightarrow> (True \<and> a)"
+  by blast
+theorem simp_asms_and_top: "(a \<and> True) \<longrightarrow> a"
+  by blast
+theorem simp_asms_top_and: "(True \<and> a) \<longrightarrow> a"
   by blast
 
-theorem simp_goal_all_top: "True --> (ALL _. True)"
+theorem simp_goal_or_top: "True \<longrightarrow> (a \<or> True)"
   by blast
-theorem simp_asms_all_top: "(ALL _. True) --> True"
+theorem simp_goal_top_or: "True \<longrightarrow> (True \<or> a)"
   by blast
-theorem simp_goal_ex_bot: "False --> (EX _. False)"
+theorem simp_asms_or_top: "(a \<or> True) \<longrightarrow> True"
   by blast
-theorem simp_asms_ex_bot: "(EX _. False) --> False"
+theorem simp_asms_top_or: "(True \<or> a) \<longrightarrow> True"
+  by blast
+
+theorem simp_goal_imp_top: "True \<longrightarrow> (a \<longrightarrow> True)"
+  by blast
+theorem simp_goal_top_imp: "a \<longrightarrow> (True \<longrightarrow> a)"
+  by blast
+theorem simp_asms_imp_top: "(a \<longrightarrow> True) \<longrightarrow> True"
+  by blast
+theorem simp_asms_top_imp: "(True \<longrightarrow> a) \<longrightarrow> a"
+  by blast
+
+theorem simp_goal_and_bot: "False \<longrightarrow> (a \<and> False)"
+  by blast
+theorem simp_goal_bot_and: "False \<longrightarrow> (False \<and> a)"
+  by blast
+theorem simp_asms_and_bot: "(a \<and> False) \<longrightarrow> False"
+  by blast
+theorem simp_asms_bot_and: "(False \<and> a) \<longrightarrow> False"
+  by blast
+
+theorem simp_goal_or_bot: "a \<longrightarrow> (a \<or> False)"
+  by blast
+theorem simp_goal_bot_or: "a \<longrightarrow> (False \<or> a)"
+  by blast
+theorem simp_asms_or_bot: "(a \<or> False) \<longrightarrow> a"
+  by blast
+theorem simp_asms_bot_or: "(False \<or> a) \<longrightarrow> a"
+  by blast
+
+theorem simp_goal_bot_imp: "True \<longrightarrow> (False \<longrightarrow> a)"
+  by blast
+theorem simp_asms_bot_imp: "(False \<longrightarrow> a) \<longrightarrow> True"
+  by blast
+
+theorem simp_goal_all_top: "True \<longrightarrow> (\<forall> _. True)"
+  by blast
+theorem simp_asms_all_top: "(\<forall> _. True) \<longrightarrow> True"
+  by blast
+theorem simp_goal_ex_bot: "False \<longrightarrow> (\<exists> _. False)"
+  by blast
+theorem simp_asms_ex_bot: "(\<exists> _. False) \<longrightarrow> False"
   by blast
 
 end
